26da9934f6c793eafba79aaca0cb6607826a990d
Pournima <pournima2496@gmail.com>
2022-08-23 17:01:12 +0530

    project completed

--- vite-project/src/components/Clients.jsx
+++ vite-project/src/components/Clients.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import {clients} from '../constants'
+import styles from '../style'
 
-const Clients = () => {
-  return (
-    <div>Clients</div>
+const Clients = () => (
+    <section id='clients' className={`${styles.flexCenter} my-4`}>
+      <div className={`${styles.flexCenter} flex-wrap w-full`}>
+        {
+          clients.map((client)=>(
+            <div key={client.id} className={`flex-1 ${styles.flexCenter} sm:min-w-[192px] min-w-[120px] client-card cursor-pointer py-3 rounded-[5px]`} >
+              <img src={client.logo} alt="client" className='sm:w-[192px] w-[100px] object-contain' />
+            </div>
+          ))
+        }
+      </div>
+    </section>
   )
-}
 
 export default Clients
--- vite-project/src/components/Navbar.jsx
+++ vite-project/src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
       <ul className='list-none sm:flex hidden justify-end items-center flex-1'>
         {navLinks.map((nav, index) => (
           <li key={nav.id}
-            className={`font-poppins font-normal cursor-pointer text-[16px] 
+            className={`font-poppins font-normal cursor-pointer text-[18px] 
           ${index === navLinks.length - 1 ? 'mr=0' : 'mr=10'} text-white mr-10`}>
             <a href={`#${nav.id}`}>{nav.title}</a>
           </li>
